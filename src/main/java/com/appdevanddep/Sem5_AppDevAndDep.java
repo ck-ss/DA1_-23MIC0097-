914884173bfd03db6e2421fcff6526583550831d
samri <samri@_Truffle>
2026-01-12 22:51:49 +0530

    Initial commit - Java Program

---
+++ src/main/java/com/appdevanddep/Sem5_AppDevAndDep.java
@@ -0,0 +1,11 @@
+package com.appdevanddep;
+
+public class Sem5_AppDevAndDep {
+
+    public static void main(String[] args) {
+        System.out.println("Samriddhi Sharma");
+        System.out.println("23MIC0097");
+        System.out.println("Java program using Jenkins CLI");
+        System.out.println("Jenkins–GitHub Integration Successful");
+    }
+}
